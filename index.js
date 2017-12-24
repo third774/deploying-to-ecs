92ea94c8df8c937d32c828e5c2b0b893c4d9fa8a
Kevin Kipp <kevin.kipp@gmail.com>
2017-12-24 00:56:24 -0600

    Serve html file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
